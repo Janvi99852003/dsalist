23c7047f46ecc1e41fda1aea0d9041b2b6d751cc
Janvi99852003 <168864863+Janvi99852003@users.noreply.github.com>
2026-01-17 19:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (7.78%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,33 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows, cols = len(matrix), len(matrix[0])
-        top, bot = 0, rows - 1
-
-        # 1️⃣ Binary search to find the correct row
-        while top <= bot:
-            row = (top + bot) // 2
-
-            if target < matrix[row][0]:
-                bot = row - 1
-            elif target > matrix[row][-1]:
-                top = row + 1
-            else:
-                break  # target must be in this row
-
-        if top > bot:
-            return False
-
-        row = (top + bot) // 2
-
-        # 2️⃣ Binary search inside the row
-        l, r = 0, cols - 1
-        while l <= r:
-            m = (l + r) // 2
-            if target < matrix[row][m]:
-                r = m - 1
-            elif target > matrix[row][m]:
-                l = m + 1
-            else:
+        m=len(matrix)
+        n=len(matrix[0])
+        t=m*n
+        l=0
+        r=t-1
+        while l<=r:
+            m=(l+r)//2
+            i=m//n
+            j=m%n
+            mid_num=matrix[i][j]
+            if target==mid_num:
                 return True
-
+            elif target<mid_num:
+                r=m-1
+            else:
+                l=m+1
         return False
