934ced6acea04ca7191e96400185cc51e0905177
Janvi99852003 <168864863+Janvi99852003@users.noreply.github.com>
2026-01-24 15:47:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (12.91%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,14 +1,10 @@
-from typing import List
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(numbers) - 1
-
-        while l < r:
-            if numbers[l] + numbers[r] > target:
-                r -= 1
-            elif numbers[l] + numbers[r] < target:
-                l += 1
+        my_hash={}
+        for i in range(0,len(numbers)):
+            comp=target-numbers[i]
+            if comp in my_hash:
+                return [my_hash[comp]+1,i+1]
             else:
-                return [l + 1, r + 1]
+                my_hash[numbers[i]]=i
+        return []
