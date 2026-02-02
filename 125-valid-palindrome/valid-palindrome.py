17878ae111a66fab0fe55de7cf8a5984dad5557c
Janvi99852003 <168864863+Janvi99852003@users.noreply.github.com>
2026-02-02 16:40:17 +0530

    Time: 11 ms (35.64%) | Memory: 19.7 MB (36.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        word = ""
-
+        st = ""
         for ch in s:
-            if ch.isalnum():          # keep letters and numbers
-                word += ch.lower()   # convert to lowercase
+            if ch.isalnum():
+                st += ch.lower()
 
         l = 0
-        r = len(word) - 1
-
+        r = len(st) - 1
         while l < r:
-            if word[l] != word[r]:
+            if st[l] != st[r]:
                 return False
             l += 1
             r -= 1
